32169a2101520b7767ba298ac81dee1d8fc4332d
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2025-01-14 17:43:54 -0300

    - Inicializar projeto WaveChat: configurar BackEnd e docker-compose com Keycloak e PostgreSQL

---
+++ src/main/java/com/jhcs/wavechat/WavechatApplication.java
@@ -0,0 +1,13 @@
+package com.jhcs.wavechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhcs/wavechat/WavechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhcs.wavechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
