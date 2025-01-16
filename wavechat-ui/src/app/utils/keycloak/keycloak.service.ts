6f203d6bd99e565d7cf8dd7aea6e1d85233dcb06
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2025-01-16 18:27:43 -0300

    Criar Serviço Keycloak e interceptor
    - Implementar o serviço Keycloak para gerenciar autenticação e autorização.
    
    - Adicionar um interceptor para interceptar requisições HTTP e adicionar tokens de autenticação.
    - Configurar serviço para inicializar no início da aplicação.
    
    - Garantir que o interceptor trate erros de autenticação e redirecione para a página de login quando necessário.

---
+++ wavechat-ui/src/app/utils/keycloak/keycloak.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import  Keycloak  from 'keycloak-js';
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakService {
+  private _keyclock :Keycloak | undefined;
+  constructor() { }
+  get keycloak() {
+    if(!this._keyclock){
+      this._keyclock = new Keycloak({
+        url: 'http://localhost:9090',
+        realm: 'wavechat',
+        clientId: 'wavechat-client'
+      });
+    }
+    return this._keyclock;
+  }
+  async init(){
+   const authenticated:boolean = await this.keycloak.init({onLoad: 'login-required'});
+  }
+  async login(){
+    await this.keycloak.login();
+  }
+  get userId():string {
+    return this.keycloak?.tokenParsed?.sub as string;
+  }
+  get isTokenValid() {
+    return !this.keycloak?.isTokenExpired() ;
+  }
+  get fullName() {
+    return this.keycloak?.tokenParsed?.['name'] as string;
+  }
+   logout(){
+    return  this.keycloak.logout({redirectUri: 'http://localhost:4200'});
+  }
+  accountMenagement(){
+    return this.keycloak.accountManagement();
+  }
+}
